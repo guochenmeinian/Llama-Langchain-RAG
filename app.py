0d40eb6ab8dc61ff2e5778e15f6d915c2654220f
Jason Yu <jy2575@nyu.edu>
2024-05-13 22:41:35 -0400

    mod app

--- app.py
+++ app.py
@@ -54,7 +54,6 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 # Function for generating LLaMA2 response
-# Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_llama2_response(prompt_input):
     string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
     
@@ -79,8 +78,6 @@ def generate_llama2_response(prompt_input):
     else:
         output = query_base(f"{string_dialogue} {prompt_input} Assistant: ")
 
-    # example code
-    #output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ", "temperature":0.1, "top_p":0.9, "max_length":512, "repetition_penalty":1})
     
     return output
 
